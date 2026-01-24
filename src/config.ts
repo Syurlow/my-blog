33b6718b20a37fcb7f1f2fb263b16a4e240f9fe3
Syurlow <1390875604@qq.com>
2026-01-24 23:58:59 +0800

    about

--- src/config.ts
+++ src/config.ts
@@ -297,12 +297,6 @@ export const navBarConfig: NavBarConfig = {
 					external: true,
 					icon: "fa6-brands:instagram",
 				},
-				{
-    				name: "QQ",
-    				icon: "fa6-brands:qq",
-    				// 尝试唤起资料卡/添加好友
-    				url: "mqqapi://card/show_pslcard?src_type=internal&version=1&uin=1390875604&card_type=person&source=sharecard",
-				},
 			],
 		},
 		{
